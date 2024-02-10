668ad2c5d16ea415bbe8980ed226fe579061dcfa
Henry Olig <olighenry@gmail.com>
2024-02-10 12:22:59 -0600

    image

--- frontend/src/components/Greeting.tsx
+++ frontend/src/components/Greeting.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './Greeting.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
+import NameImg from '../images/myname.png'
 
 const App = () => {
     return(
         <div className='GreetContainer'>
             <h4>Hello, my name is</h4>
-            <h2>Henry Olig</h2>
+            <img className='name' src={NameImg} alt='Henry Olig'/>
             <h4>CS [AT] UNIVERSITY OF MINNESOTA</h4>
             <h4>HENRY [AT] OLIG [DOT] DEV</h4>
             

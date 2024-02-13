4095b62816a465c159982809607b089a9beab5ed
Henry Olig <olighenry@gmail.com>
2024-02-13 12:16:55 -0600

    mobile optimization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,69 +3,23 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Greeting from './components/Greeting'
 import { Center } from '@chakra-ui/layout';
-import { Image, Container, Button } from '@chakra-ui/react'
-import Navvy from './components/Navvy'
-import BlogBtn from './images/blogbtn.png'
-import WorkBtn from './images/myworkbtn.png'
-import AboutBtn from './images/aboutmebtn.png'
-import Work from './mywork/work-experience';
-import ActiveWorkBtn from './images/activeWorkBtn.png'
-import ActiveAbtBtn from './images/activeAbtMeBtn.png'
-import ActiveBlogBtn from './images/activeBlogBtn.png'
+import { Hide, Image, Show } from '@chakra-ui/react'
 import Sides from './images/sides.png'
 import Rside from './images/right.png'
 import Lside from './images/left.png'
 
+
 const App = () => {
-
-  const [workimage, setWorkImage] = useState(WorkBtn);
-  const [aboutimage, setAboutImage] = useState(AboutBtn);
-  const [blogimage, setBlogImage] = useState(BlogBtn);
-
 
   return (
     <div className='home' color='#eeffff'>
-      <Image src={Lside} mr='auto' w='35vw' h='100vh' />
-      <Center w="25vw" rounded='md' mt='5vh'>
-        <div className='greeting'>
-          <Greeting></Greeting>
-          <div className='senders'>
-            <Link to='/hobbies'>
-              <Image src={aboutimage}
-                onMouseEnter={() => setAboutImage(ActiveAbtBtn)}
-                onMouseOut={() => setAboutImage(AboutBtn)}
-                width='20vw'
-                ml='auto'
-                mr='auto'
-                cursor='pointer'
-              />
-            </Link>
-            <Link to='/work-experience'>
-              <Image src={workimage}
-                onMouseEnter={() => setWorkImage(ActiveWorkBtn)}
-                onMouseOut={() => setWorkImage(WorkBtn)}
-                width='16vw'
-                ml='auto'
-                mr='auto'
-                mt='1vh'
-                cursor='pointer'
-              />
-            </Link>
-            <Link to='/blog'>
-              <Image src={blogimage}
-                onMouseEnter={() => setBlogImage(ActiveBlogBtn)}
-                onMouseOut={() => setBlogImage(BlogBtn)}
-                width='10vw'
-                ml='auto'
-                mr='auto'
-                mt='1vh'
-                cursor='pointer'
-              />
-            </Link>
-          </div>
-        </div>
-      </Center>
-      <Image src={Rside} ml='auto' w='35vw' h='100vh' />
+      <Hide below='lg'>
+        <Image src={Lside} mr='auto' w='35vw' h='100vh' />
+      </Hide>
+      <Greeting></Greeting>
+      <Hide below='lg'>
+        <Image src={Rside} ml='auto' w='35vw' h='100vh' />
+      </Hide>
     </div>
   );
 }
--- frontend/src/components/Greeting.tsx
+++ frontend/src/components/Greeting.tsx
@@ -1,19 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './Greeting.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Image, Container } from '@chakra-ui/react'
+import { Link } from 'react-router-dom';
+import { Image, Container, Center } from '@chakra-ui/react'
 import NameImg from '../images/myname.png'
+import BlogBtn from '../images/blogbtn.png'
+import WorkBtn from '../images/myworkbtn.png'
+import AboutBtn from '../images/aboutmebtn.png'
+import ActiveWorkBtn from '../images/activeWorkBtn.png'
+import ActiveAbtBtn from '../images/activeAbtMeBtn.png'
+import ActiveBlogBtn from '../images/activeBlogBtn.png'
+import Top from '../images/top.png'
+import MeDrawing from '../images/medrawing1.png'
+import Bottom from '../images/bottom.png'
 
 const App = () => {
+    const [workimage, setWorkImage] = useState(WorkBtn);
+    const [aboutimage, setAboutImage] = useState(AboutBtn);
+    const [blogimage, setBlogImage] = useState(BlogBtn);
     return(
-        <Container minW='45vw' color='#eefff' className='GreetContainer'>
-            <h4>Hello, my name is</h4>
-            <Image className='name' src={NameImg} alt='Henry Olig'></Image>
-            <h4>CS [AT] UNIVERSITY OF MINNESOTA</h4>
-            <h4>HENRY [AT] OLIG [DOT] DEV</h4>
-            
-        </Container>
+        <Center ml='auto' mr='auto' minW='400px' justifyContent='space-between' h='100vh' w="25vw" rounded='md' pt='0' mt='0'>
+            <div className='greeting'>
+                <Image minW='175px' w='15vw' src={Top} ml='auto' mr='auto' />
+                    <Container m='0' minW='25vw' color='#eefff' className='GreetContainer' p='0'>
+                    <Image minW='150px' maxW='700px' w='20vw' src={MeDrawing} ml='auto' mr='auto' />
+                        {/* <h4>Hello, my name is</h4> */}
+                        <Image minW='275px' mb='5vh' className='name' src={NameImg} alt='Henry Olig'></Image>
+                        {/* <h4>CS [AT] UNIVERSITY OF MINNESOTA</h4>
+                        <h4>HENRY [AT] OLIG [DOT] DEV</h4> */}
+                        <div className='senders'>
+                            <Link to='/hobbies'>
+                            <Image src={aboutimage}
+                                onMouseEnter={() => setAboutImage(ActiveAbtBtn)}
+                                onMouseOut={() => setAboutImage(AboutBtn)}
+                                width='20vw'
+                                minW='250px'
+                                ml='auto'
+                                mr='auto'
+                                cursor='pointer'
+                            />
+                            </Link>
+                            <Link to='/work-experience'>
+                            <Image src={workimage}
+                                onMouseEnter={() => setWorkImage(ActiveWorkBtn)}
+                                onMouseOut={() => setWorkImage(WorkBtn)}
+                                width='16vw'
+                                minW='195px'
+                                ml='auto'
+                                mr='auto'
+                                mt='1vh'
+                                cursor='pointer'
+                            />
+                            </Link>
+                            <Link to='/blog'>
+                            <Image src={blogimage}
+                                onMouseEnter={() => setBlogImage(ActiveBlogBtn)}
+                                onMouseOut={() => setBlogImage(BlogBtn)}
+                                width='10.5vw'
+                                minW='125px'
+                                ml='auto'
+                                mr='auto'
+                                mt='1vh'
+                                cursor='pointer'
+                                mb='10vh'
+                            />
+                            </Link>
+                        </div>
+                    </Container>
+                <Image minW='250px' w='22vw' src={Bottom} ml='auto' mr='auto'/>
+            </div>
+        </Center>
     );
 }
 

e1c13a09e7b9d9f94ffa4ad0b94c085d3747057e
Henry Olig <olighenry@gmail.com>
2024-02-23 11:16:35 -0600

    work mode

--- frontend/src/mywork/work-experience.tsx
+++ frontend/src/mywork/work-experience.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Navvy from '../components/Navvy';
 import { Center, Container, Text } from '@chakra-ui/react';
 
 const Work = () => {
+
   const [serious, setSeriousness] = useState(false);
+  useEffect(() => {
+    let d = new Date();
+    let time = d.getHours();  
+    if (time < 9 || time > 17) {
+      setSeriousness(false);
+    }
+    else {
+      setSeriousness(true);
+    }
+  }, []);
+
 
   if (serious == false) {
     return (
@@ -19,7 +31,9 @@ const Work = () => {
   else {
     return (
       <div>
-        <Container m='0' p='0' minW='100vw' minH='100vh' backgroundColor={'#323232'}></Container>
+        <Center m='0' p='0' minW='100vw' minH='100vh' backgroundColor={'#323232'}>
+          <Text color={'#FFFFFF'} fontSize={'3vw'} onClick={() => setSeriousness(false)}>I love work</Text>
+        </Center>
       </div>
     );
     }
